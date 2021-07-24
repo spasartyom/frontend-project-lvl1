0db50b81bebda3cc5389beabc98b0dd56dc02243
Artem Spas <spasartyom@gmail.com>
2021-07-24 22:30:13 +0300

    feat: complete step 3

---
+++ src/cli.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync';
+
+const makeGreetings = () => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+}
+
+export default makeGreetings;
+
+// makeGreetings();
