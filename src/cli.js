8f805953efa796371ae46435cf76f5f7c6eedd6c
Artem Spas <spasartyom@gmail.com>
2021-07-25 14:37:04 +0300

    feat: setup eslint

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export const makeGreetings = () => {
-    console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
-}
+const makeGreetings = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
+
+export default makeGreetings;
